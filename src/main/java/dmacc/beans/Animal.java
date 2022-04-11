266c5c20831a283a5eb4b5675c4bbacd96d7f7fe
Tim Ancona <tsancona@dmacc.edu>
2022-04-11 17:54:53 -0500

    changed Animal class to use Lombok

--- src/main/java/dmacc/beans/Animal.java
+++ src/main/java/dmacc/beans/Animal.java
@@ -4,8 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Animal {
 	@Id
 	@GeneratedValue
@@ -16,57 +21,6 @@ public class Animal {
 	public String size;
 	public String color;
 	public String image;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getImage() {
-		return image;
-	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-	
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	
-	public String getSpecies() {
-		return species;
-	}
-	public void setSpecies(String species) {
-		this.species = species;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getBreed() {
-		return breed;
-	}
-	public void setBreed(String breed) {
-		this.breed = breed;
-	}
-	public String getSize() {
-		return size;
-	}
-	public void setSize(String size) {
-		this.size = size;
-	}
-	
-	public Animal() {
-		super();
-	}
-	
 	
 	public Animal(String species, String name, String breed, String size) {
 		super();
@@ -87,11 +41,6 @@ public class Animal {
 		this.size = size;
 		this.color = color;
 		this.image = image;
-	}
-	
-	@Override
-	public String toString() {
-		return "Animal [species=" + species + ", name=" + name + ", breed=" + breed + ", size=" + size + "]";
 	}
 	
 }
