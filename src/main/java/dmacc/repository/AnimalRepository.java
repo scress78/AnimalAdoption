6a3b8551c6530a135995352b7934debf571bbcd2
tjmendenhall <tjmendenhall2@dmacc.edu>
2022-04-20 15:55:39 -0500

    Search all animals by keyword

--- src/main/java/dmacc/repository/AnimalRepository.java
+++ src/main/java/dmacc/repository/AnimalRepository.java
@@ -3,6 +3,7 @@ package dmacc.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dmacc.beans.Animal;
@@ -14,5 +15,8 @@ public interface AnimalRepository extends JpaRepository<Animal, Long>{
 	 * @return
 	 */
 	List<Animal> findBySpecies(String species);
+	
+	@Query("SELECT a FROM Animal a WHERE CONCAT(a.species, ' ', a.name, ' ', a.breed, ' ', a.size, ' ', a.color) LIKE %?1%")
+	public List<Animal> searchAnimal(String keyword);
 
 }
