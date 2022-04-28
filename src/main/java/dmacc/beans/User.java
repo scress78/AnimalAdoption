42c4c8971191531d985596a005c89b1ac1f6caca
Tim Ancona <tsancona@dmacc.edu>
2022-04-27 19:58:41 -0500

    changed classes to use lombok

--- src/main/java/dmacc/beans/User.java
+++ src/main/java/dmacc/beans/User.java
@@ -23,75 +23,8 @@ public class User {
 	private String userName;
 	private String password;
 	private boolean loggedIn;
-
 	@ManyToMany
 	private List<Animal> favorites;
-
-	
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", favorites=" + favorites
-				+ "]";
-	}
-	
-	/*
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", userName=" + userName + ", password=" + password+ "]";
-	}
-	*/
-	
-	/*
-	public long getId() {
-		return id;
-	}
-
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-
-	public String getUserName() {
-		return userName;
-	}
-
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-	
-	public List<Animal> getFavorites() {
-		return favorites;
-	}
-
-
-	public void setFavorites(List<Animal> favorites) {
-		this.favorites = favorites;
-	}
-
-	*/
-
-	
-	
-	
-	
-	public boolean isLoggedIn() {
-		return loggedIn;
-	}
-
-	public void setLoggedIn(boolean loggedIn) {
-		this.loggedIn = loggedIn;
-	}
-	
 	
 	public String logOut() {
 		if(isLoggedIn() == false) {
@@ -118,13 +51,6 @@ public class User {
 		}
 	}
 	
-	
-
-	public User() {
-		super();
-	}
-
-
 	public User(long id, String userName, String password) {
 		super();
 		this.id = id;
